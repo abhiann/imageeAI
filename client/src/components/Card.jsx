714638fc4f0d719aea83321122b44cd42083ac20
Abhishek Ranjan <abhishekrjnn@gmail.com>
2023-03-21 10:29:21 +0530

    cards styling

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({_id, name, prompt, photo}) => {
   return (
     <div className = "rounded-xl group relative shadow-card hover:shadow-cardhover card">
       <img 
-        className='w-full h-auto object-cover rounder-xl'
+        className='w-full h-auto object-cover rounded-xl'
         src = {photo}
         alt={prompt}
       />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ const RenderCards = ({data, title}) => {
   }
 
   return (
-    <h2 className='mt-5 font-bold text-[#6449ff] text-x1 uppercase'>{title}</h2>
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
   )
 }
 
@@ -61,7 +61,7 @@ const Home = () => {
           {searchText && (
             <h2 className='font-medium text-[#666e75] text-x1 mb-3'> Showing results for <span className='text-[#222328]'>{searchText}</span></h2>
           )}
-            <div className=' = "grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+            <div className = "grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText? (
                 <RenderCards
                   data={[]}
